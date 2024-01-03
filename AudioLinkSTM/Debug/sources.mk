c62a40292ded72e4ecb488bc5c387965647ea7e6
Antonio <antoniosanmu100@gmail.com>
2024-01-03 21:09:54 +0100

    Insertado el proyecto para el STM

---
+++ AudioLinkSTM/Debug/sources.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+ELF_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+SIZE_OUTPUT := 
+OBJDUMP_LIST := 
+SU_FILES := 
+EXECUTABLES := 
+OBJS := 
+MAP_FILES := 
+S_DEPS := 
+S_UPPER_DEPS := 
+C_DEPS := 
+OBJCOPY_BIN := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+Doc \
+Drivers/BSP/Components/ft5336 \
+Drivers/BSP/Components/wm8994 \
+Drivers/BSP/STM32F7508-Discovery \
+Drivers/STM32F7xx_HAL_Driver/Src \
+Middlewares/ST/STM32_USB_Host_Library/Class/MSC/Src \
+Middlewares/ST/STM32_USB_Host_Library/Core/Src \
+Middlewares/Third_Party/FatFs/src \
+Middlewares/Third_Party/FatFs/src/option \
+Src \
+Utilities/Log \
+
